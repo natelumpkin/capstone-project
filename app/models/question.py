5b457db89f244bd01a4f26b0e2f161974ef03833
Nate Lumpkin <nate.lumpkin@gmail.com>
2022-11-22 09:33:13 -0800

    question model

---
+++ app/models/question.py
@@ -0,0 +1,17 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Question(db.Model):
+  __tablename__ = "questions"
+
+  if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
+  title = db.Column(db.String(100))
+  body = db.Column(db.Text())
+  created_at = db.Column(db.DateTime(), default=datetime.utcnow())
+  updated_at = db.Column(db.DateTime(), default=datetime.utcnow())
+
+  author = db.relationship('User', back_populates="questions")
