68590cb649f038deae2ea446990a3c1bdd0fc6df
Nate Lumpkin <nate.lumpkin@gmail.com>
2022-11-22 10:20:35 -0800

    modified seeder for users data

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -3,16 +3,16 @@ from app.models import db, User, environment, SCHEMA
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+    demo1 = User(
+        username='Demo1', email='demouser1@email.com', password='password')
+    demo2 = User(
+        username='Demo2', email='demouser2@email.com', password='password')
+    demo3 = User(
+        username='Demo2', email='demouser3@email.com', password='password')
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(demo1)
+    db.session.add(demo2)
+    db.session.add(demo3)
     db.session.commit()
 
 
@@ -27,5 +27,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute("DELETE FROM users")
-        
+
     db.session.commit()
