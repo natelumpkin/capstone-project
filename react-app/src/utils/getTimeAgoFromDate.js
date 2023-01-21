3a3c1ea09276ff57609671bedbcfcf8961852704
Nate Lumpkin <nate.lumpkin@gmail.com>
2023-01-20 20:06:41 -0800

    fixing date bug

--- react-app/src/utils/getTimeAgoFromDate.js
+++ react-app/src/utils/getTimeAgoFromDate.js
@@ -25,6 +25,62 @@ const getTimeAgoFromDate = (dateString, updatedString) => {
     postTime = updatedTime
   }
 
+  let timeBetween = currentTime - postTime;
+  // console.log('time between: ', timeBetween)
+
+  // console.log('')
+  let oneYear = 31556952000;
+  let oneMonth = Math.floor(oneYear / 12);
+  let oneDay = 86400000;
+  let oneHour = 3600000;
+  let oneMinute = 60000;
+  let oneSecond = 1000;
+
+  let yearUnit;
+  let monthUnit;
+  let dayUnit;
+  let hourUnit;
+  let minuteUnit;
+  let secondUnit;
+
+  // if (timeBetween <= oneMinute) {
+  //   let seconds = Math.floor(timeBetween / oneSecond)
+  //   seconds === 1 ? secondUnit = 'seconds' : secondUnit = 'seconds'
+  //   return `${verb}${seconds} ${secondUnit} ago`
+  // } else if (timeBetween <= oneHour) {
+  //   let minutes = Math.floor(timeBetween / oneMinute)
+  //   let seconds = Math.floor((timeBetween - (oneMinute * minutes)) / oneSecond)
+  //   seconds === 1 ? secondUnit = 'second' : secondUnit = 'seconds'
+  //   minutes === 1 ? minuteUnit = 'minute' : minuteUnit = 'minutes'
+  //   return `${verb}${minutes} ${minuteUnit}, ${seconds} ${secondUnit} ago`
+  // } else if (timeBetween <= oneDay) {
+  //   let hours = Math.floor(timeBetween / oneHour)
+  //   let minutes = Math.floor((timeBetween - (oneHour * hours)) / oneMinute)
+  //   hours === 1 ? hourUnit = 'hour' : dayUnit = 'hours'
+  //   minutes === 1 ? minuteUnit = 'minute' : minuteUnit = 'minutes'
+  //   return `${verb}${hours} ${hourUnit}, ${minutes} ${minuteUnit} ago`
+  // } else if (timeBetween <= oneMonth) {
+  //   let days = Math.floor(timeBetween / oneDay)
+  //   let hours = Math.floor((timeBetween - (oneDay * days)) / oneHour)
+  //   days === 1 ? dayUnit = 'day' : dayUnit = 'days'
+  //   hours === 1 ? hourUnit = 'hour' : hourUnit = 'hours'
+  //   return `${verb}${days} ${dayUnit}, ${hours} ${hourUnit} ago`
+  // } else if (timeBetween <= oneYear) {
+  //   let months = Math.floor(timeBetween / oneMonth)
+  //   let days = Math.floor((timeBetween - (oneMonth * months)) / oneDay)
+  //   // console.log(months, ' months')
+  //   // console.log(days, ' remaining days')
+  //   months === 1 ? monthUnit = 'month' : monthUnit = 'months'
+  //   days === 1 ? dayUnit = 'day' : dayUnit = 'days'
+  //   return `${verb}${months} ${monthUnit}, ${days} ${dayUnit} ago`
+  // } else {
+  //   let years = Math.floor(timeBetween / oneYear)
+  //   let months = Math.floor((timeBetween - (oneYear * years)) / oneMonth)
+  //   years === 1 ? yearUnit = 'year' : yearUnit = 'years'
+  //   months === 1 ? monthUnit = 'month' : yearUnit = 'years'
+  //   return `${verb}${years} ${yearUnit}, ${months} ${monthUnit} ago`
+  // }
+
   const year = postTime.getFullYear()
   const month = postTime.getMonth()
   const day = postTime.getDate()
