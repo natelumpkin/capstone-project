cb4aa168f41627722d70ee16916cc5dd93f7c3d6
Nate Lumpkin <nate.lumpkin@gmail.com>
2023-03-02 12:54:48 -0800

    styling filters

--- react-app/src/components/FilterChooser/index.js
+++ react-app/src/components/FilterChooser/index.js
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
 
+import './FilterChooser.css'
+
 const FilterChooser = () => {
 
   // each button is a link that brings you to a new URL
@@ -8,12 +10,24 @@ const FilterChooser = () => {
 
   // but I don't think we need to worry about those yet
 
+  // when we click, we need to remove active from any active elements
+  // and add it to the one we just clicked
+
+  const toggleActive = (e) => {
+    const filters = document.getElementsByClassName("filter-link")
+    for (let link of filters) {
+      link.classList.remove("active-filter")
+    }
+    const curr = e.target
+    curr.classList.add("active-filter")
+  }
+
   return (
-    <div>
-      <NavLink to='/questions'>Newest</NavLink>
-      <NavLink to='/questions?tab=recent'>Recent</NavLink>
-      <NavLink to='/questions?unanswered=true'>Unanswered</NavLink>
-      <NavLink to='/questions?tab=score'>Score</NavLink>
+    <div id="filter-chooser">
+      <NavLink onClick={(e) => toggleActive(e)} className="filter-link border-left" id="newest" to='/questions' exact={true}>Newest</NavLink>
+      <NavLink onClick={(e) => toggleActive(e)} className="filter-link" id="recent" to='/questions?tab=recent'>Recent Activity</NavLink>
+      <NavLink onClick={(e) => toggleActive(e)} className="filter-link" id="unanswered" to='/questions?unanswered=true'>Unanswered</NavLink>
+      <NavLink onClick={(e) => toggleActive(e)} id="score" className='border-right filter-link' to='/questions?tab=score'>High Score</NavLink>
     </div>
   )
 }
